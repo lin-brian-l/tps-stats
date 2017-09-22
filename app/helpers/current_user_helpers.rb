9804c60acc2a6f49b585776e4c5232c6806a439e
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-09-21 20:15:37 -0500

    update current user helper method.

--- app/helpers/current_user_helpers.rb
+++ app/helpers/current_user_helpers.rb
@@ -1,6 +1,10 @@
 module CurrentUser
   def current_user
-    current_user ||= User.find(session[:user_id])
+    if session[:user_id] != nil
+      current_user = User.find(session[:user_id])
+    else
+      nil
+    end
   end
 end
 
