20abae692f698b230d6f5171b054015ca077e8d6
Katherine Thongsakounh <kthongsakounh@gmail.com>
2017-10-03 10:15:28 -0500

    change route in user controller to redirect to login page after registering

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -7,7 +7,7 @@ post '/users' do
   @user = User.new(params[:user])
 
   if @user.save
-    redirect '/'
+    redirect '/sessions/new'
   else
     @errors = @user.errors.full_messages
     erb :"users/new"
