d9d5353e6eb4e7ed5897158a2e56b63a3ab096a6
lin-brian-l <lin.brian.l@gmail.com>
2018-03-12 00:55:48 -0500

    Begin writing js to add new tournaments.

--- app/controllers/tournaments.rb
+++ app/controllers/tournaments.rb
@@ -3,6 +3,11 @@ get '/tournaments' do
   erb :'tournaments/index'
 end
 
+get '/tournaments/new' do
+  @tournament = Tournament.new
+  erb :'tournaments/new'  
+end
+
 get '/tournaments/:id' do
   @tournament = Tournament.find_by(id: params[:id])
   erb :'tournaments/show'  
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,61 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  console.log("plz load");
+  $("#submit-link").on("click", function() {
+    console.log("plz work");
+  });
 });
+
+// $(document).ready(function() {
+//   $("form").on("submit", function() {
+//     event.preventDefault();
+//     var $form = $(this);
+//     var method = $form.attr("method");
+//     var url = $form.attr("action");
+//     var request = $.ajax({
+//       method: method,
+//       url: url
+//     });
+
+//     request.done(function(response) {
+//       $("main").append(response);
+//       $form.hide();
+//     });
+//   });
+
+//   $("main").on("submit", "form#new-horse-form", function() {
+//     event.preventDefault();
+//     var $form = $(this);
+//     var method = $form.attr("method");
+//     var url = $form.attr("action");
+//     var data = $form.serialize();
+
+//     var request = $.ajax({
+//       method: method,
+//       url: url,
+//       data: data
+//     });
+
+//     request.done(function(response) {
+//       $("ul").append(response);
+//       $form.hide();
+//       $("form#add").show();
+//     });
+//   });
+
+//   $("main").on("click", "a", function() {
+//     event.preventDefault();
+//     var $link = $(this);
+//     var method = "GET";
+//     var url = $link.attr("href");
+
+//     var request = $.ajax({
+//       method: method,
+//       url: url
+//     });
+
+//     request.done(function(response) {
+//       $link.after(response);
+//     });
+//   });
+
+// });
